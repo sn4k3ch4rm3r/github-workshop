43197c7b29806609f79ae8573f79c3bcb20a1b57
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2024-04-15 13:42:33 +0200

    Added some code

---
+++ code/script.js
@@ -0,0 +1,39 @@
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+function makeMove(index) {
+	if (gameActive && board[index] === '') {
+		board[index] = currentPlayer;
+		document.getElementsByClassName('cell')[index].innerText = currentPlayer;
+		checkWin();
+		togglePlayer();
+	}
+}
+
+function togglePlayer() {
+	currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWin() {
+	const winningConditions = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+		[0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+		[0, 4, 8], [2, 4, 6] // Diagonals
+	];
+
+	for (let condition of winningConditions) {
+		const [a, b, c] = condition;
+		if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+			gameActive = false;
+			alert(`${currentPlayer} wins!`);
+		}
+	}
+}
+
+function resetGame() {
+	currentPlayer = 'X';
+	board = ['', '', '', '', '', '', '', '', ''];
+	gameActive = true;
+	document.querySelectorAll('.cell').forEach(cell => cell.innerText = '');
+}
